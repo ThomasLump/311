c1746469855821a5c5130d515de9c7efc04bc096
ThomasLump <thomaslump@hotmail.com>
2025-02-09 17:24:52 +0300

    312

--- src/main/java/web/controller/UsersEditController.java
+++ src/main/java/web/controller/UsersEditController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.data.User;
+import web.model.User;
 import web.service.UserCrudService;
 
 
