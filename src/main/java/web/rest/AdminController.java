d3f5991418d81be42eaf5df90b3d77fa2eccb706
ThomasLump <thomaslump@hotmail.com>
2025-03-10 22:34:36 +0300

    init

--- src/main/java/web/rest/AdminController.java
+++ src/main/java/web/rest/AdminController.java
@@ -2,9 +2,7 @@ package web.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import web.dto.UserDto;
 import web.service.RoleService;
 import web.service.UserServiceCrud;
@@ -12,21 +10,38 @@ import web.service.UserServiceCrud;
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
-    UserServiceCrud userServiceCrud;
+    UserServiceCrud userService;
     RoleService roleService;
 
     @Autowired
     public AdminController(RoleService roleService, UserServiceCrud userServiceCrud) {
         this.roleService = roleService;
-        this.userServiceCrud = userServiceCrud;
+        this.userService = userServiceCrud;
     }
 
     @GetMapping("/users")
     public ResponseEntity<Iterable<UserDto>> getAllUsers() {
-        Iterable<UserDto> users = userServiceCrud.getAllUsers();
+        Iterable<UserDto> users = userService.getAllUsers();
          return ResponseEntity.ok(users);
     }
 
+    @GetMapping("/add")
+    public ResponseEntity<Void> addUser(@RequestBody UserDto userDto) {
+        userService.addUserByDto(userDto);
+        return ResponseEntity.ok().build();
+    }
 
+    @GetMapping("/update")
+    public ResponseEntity<Void> updateUser(@RequestBody UserDto userDto) {
+        userService.updateUserByDto(userDto);
+        return ResponseEntity.ok().build();
+    }
 
+    @GetMapping("/delete")
+    public ResponseEntity<Void> deleteUser(@RequestBody Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    //test
 }
